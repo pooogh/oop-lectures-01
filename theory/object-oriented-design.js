04498bfc2de8a2a831d997dec6ba3a5f9eb8a4e5
Lucy <pooogh9@gmail.com>
2024-11-06 14:04:52 +0300

    update 06.11

--- theory/object-oriented-design.js
+++ theory/object-oriented-design.js
@@ -62,3 +62,87 @@ console.log(validator2.validateEmail('vanya@2005.ru', {hasDot: false}));
 // объект - набор св-в валидации + методы валидации по св-вам любой регистрации
 
 // console.log(Math.random() * 8)
+
+// плавающий интерфейс и сборщики
+class Gnomik {
+    constructor() {
+        this.height = 50 + Math.round(Math.random() * 100);
+        this.entity = true;
+        this.age = 100 + Math.round(Math.random() * 100);
+    };
+
+    changeEntity() {
+        this.entity = !this.entity;
+        // this.name = 'Oleg';
+        return this;
+    };
+
+    changeHeight() {
+        this.height = 50 + Math.round(Math.random() * 100);
+        return this;
+    };
+};
+
+const almazIndigo = new Gnomik();
+console.log(almazIndigo);
+console.log(almazIndigo.changeEntity().changeEntity().changeHeight());
+// almazIndigo.changeHeight();
+console.log(almazIndigo);
+
+class Kolduni {
+    constructor(list) {
+        this.list = list;
+    };
+
+    filterByZdorovie() {
+        const ans = this.list.filter(({zdorovie}) => zdorovie > 7);
+        return new Kolduni(ans);
+    };
+
+    filterByAge() {
+        const ans = this.list.filter(({age}) => age > 55);
+        return new Kolduni(ans);
+    };
+
+    addPandus() {
+        this.list.forEach((koldun) => koldun.pandus = koldun.iq < 10);
+    }
+}
+
+
+const getRandom = (mn, delta) => {
+    return mn + Math.round(Math.random() * delta);
+}
+const listOfKolduni = [
+    {name: 'Denis', age: getRandom(12, 1000), zdorovie: getRandom(1, 25), mana: getRandom(100, 1000), iq: 10},
+    {name: 'Ivan', age: getRandom(50, 1000), zdorovie: getRandom(1, 3), mana: getRandom(0, 10), iq: 9},
+    {name: 'Anna', age: getRandom(70, 25), zdorovie: getRandom(2, 10), mana: getRandom(1000, 2), iq: 116},
+    {name: 'Timofey', age: getRandom(21, 10), zdorovie: getRandom(1, 22), mana: getRandom(1001, 2), iq: getRandom(1, 15)},
+    {name: 'Yana', age: getRandom(2, 3), zdorovie: getRandom(2, 3), mana: getRandom(1002, 2), iq: getRandom(100, 50)},
+]
+
+const kolduns = new Kolduni(listOfKolduni);
+kolduns.addPandus();
+console.log(kolduns);
+const filteredKolduns = kolduns.filterByZdorovie().filterByAge();
+console.log(kolduns);
+console.log(filteredKolduns);
+
+
+class ValidateForDiddy {
+    static def = {
+        age: 20,
+        zdorovie: 7,
+    };
+
+    constructor(props = {}) {
+        this.options = {...this.constructor.def, ...props};
+    };
+
+    age(obj, prev = true) {
+        return obj.age > this.options.age ?? prev;
+    };
+};
+
+const test = new ValidateForDiddy();
+console.log(test.age(listOfKolduni.at(0)).zdorovie());
